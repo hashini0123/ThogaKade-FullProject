dd577ff33f525fb1f47ef41534131f72ddcf460a
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-25 10:17:52 +0530

    first commit

---
+++ src/main/java/edu/icet/model/dto/CustomerDTO.java
@@ -0,0 +1,19 @@
+package edu.icet.model.dto;
+
+import java.time.LocalDate;
+
+public class CustomerDTO {
+
+    private String custID;
+
+    private String custTitle;
+
+    private String custName;
+
+    private LocalDate DOB;
+
+    private double salary;
+
+
+
+}
