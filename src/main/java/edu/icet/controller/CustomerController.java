c792fa1e4d62cbe853626ee46db316ff3fda994b
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-25 15:09:03 +0530

    commit #4

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,21 +1,24 @@
 package edu.icet.controller;
 
 import edu.icet.model.Customer;
-import edu.icet.service.CustomerService;
+import edu.icet.service.Impl.CustomerServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CustomerController {
 
-    public CustomerService customerService =new CustomerService();
+    public CustomerServiceImpl customerService =new CustomerServiceImpl();
 
     @GetMapping("/customer")
     public List<Customer> getAllCustomer(){
        return   customerService.getAllCustomer();
     }
 
+    @GetMapping("/add")
+    public void addCustomer(){
+        return customerService.addCustomer();
+    }
 }
