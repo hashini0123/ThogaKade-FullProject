7085ee65ba682122974fd7859e0fa7d54d09e7a5
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-26 11:23:54 +0530

    commit #13

--- src/main/java/edu/icet/service/Impl/CustomerServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/CustomerServiceImpl.java
@@ -1,21 +1,34 @@
 package edu.icet.service.Impl;
 import edu.icet.model.Customer;
-import edu.icet.repository.CustomerRepository;
+import edu.icet.repository.CustomerRepositoryImpl;
 import edu.icet.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new CustomerRepository();
+    @Autowired
+    CustomerRepositoryImpl customerRepository;
 
     @Override
     public List<Customer> getAllCustomer() {
-        return customerRepository.getCustomerList();
+        return customerRepository.getAllCustomer();
+    }
+
+    @Override
+    public List<String> stringList(){
+        return customerRepository.stringList();
     }
 
     @Override
     public void addCustomer(Customer customer) {
+    }
 
+    @Override
+    public void updateCustomer(Customer customer) {
+        return customerRepository.updateCustomer();
     }
 }
